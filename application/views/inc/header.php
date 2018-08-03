c0385f347a8d74bed93ae278d45e2975a70c3ca0
Krzysztof Ruczkowski <tojatos@gmail.com>
2018-08-03 09:02:25 +0200

    css and js initialization

--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -2,9 +2,9 @@
 <!DOCTYPE html>
 <html>
   <head>
-	<link rel="stylesheet" href="<?= site_url('public/css/main.css') ?>">
-	<link href="<?= site_url('public/images/favico.ico') ?>" rel="shortcut icon" type="image/icon" />
-	<script src="<?= site_url('public/js/main.js') ?>"></script>
+	<link rel="stylesheet" href="<?= base_url('public/main.css') ?>">
+	<link href="<?= base_url('public/favico.ico') ?>" rel="shortcut icon" type="image/icon" />
+	<script src="<?= base_url('public/main.js') ?>"></script>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>NKM<?= isset($title) ? " - ".$title : "" ?></title>
