beb0bdf7c369e7d32363e1f0c4d211865102f2fc
Krzysztof Ruczkowski <tojatos@gmail.com>
2018-08-05 06:32:59 +0200

    Main views and routes added

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -1,11 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Main extends MY_controller{
-
-	public function index()
+class Main extends MY_controller
+{
+	public function index($content_name = "main")
 	{
-		$this->load->helper('url');
-		$this->show_main_view(['content' => $this->get_view("main")]);
+		$this->show_main_view(['content' => $this->get_view($content_name)]);
 	}
 }
