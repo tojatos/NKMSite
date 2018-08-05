088ca72dd3232797fc809cf7c3677136b28a7451
Krzysztof Ruczkowski <tojatos@gmail.com>
2018-08-05 03:52:01 +0200

    Navbar links; Files grouped in folders

--- application/views/inc/main_nav.php
+++ application/views/inc/main_nav.php
@@ -3,6 +3,10 @@
   <div class="left">
 	<a href=<?= base_url() ?> class="nkm_logo"></a>
   </div>
-  <div class="center"></div>
+  <div class="center">
+	<a href=<?= base_url("About") ?> class="nav_link">O grze</a>
+	<a href=<?= base_url("Gallery") ?> class="nav_link">Galeria</a>
+	<a href=<?= base_url("CharacterQueue") ?> class="nav_link">Kolejka postaci</a>
+  </div>
   <div class="right"></div>
 </nav>
